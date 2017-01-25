27d479a886c9ea01fd6011f60bddc2ae9fc06219
Jbedwell91 <jpbedwell5@sbcglobal.net>
2017-01-24 22:22:38 -0500

    Delete jquery.skip.js

--- pcllab-conceptmap/lib/jquery.skip.js
+++
@@ -1,9 +0,0 @@
-$.ctrl = function(key, callback, args) {
-	$(document).keydown(function(e) {
-		if(!args) args=[]; 
-		if(e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
-			callback.apply(this, args);
-			return false;
-		}
-	});        
-};
